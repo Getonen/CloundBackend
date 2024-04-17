af30afe1b70fd6e54937067b57ad8c0791d542d6
Getonen <87571016+Getonen@users.noreply.github.com>
2024-04-17 10:19:01 +0300

    Update app.py
    test for rahti

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! small change here for test</p>"
 
 if  __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port = 8080) #4x0 answers for any user
