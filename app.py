8a8851eadb74a092d836007634e1a4f58986250a
Getonen <87571016+Getonen@users.noreply.github.com>
2024-04-17 12:18:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! small change here for test</p>"
+def get_data():
+    data = {'message':'FLASKIN VIESTI!'}
+    return jsonify(data)
 
 if  __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port = 8080) #4x0 answers for any user
